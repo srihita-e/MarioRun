8cbec26624b90de0ea0020920f854653b758e377
srihita-e <77421310+srihita-e@users.noreply.github.com>
2021-04-26 21:25:05 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+img = "";
+noseX = "";
+noseY = "";
+GameStatus = "";
+marioX = 325;
+marioY = 325;
+
+function startGame()
+{
+	GameStatus = "start";
+	document.getElementById("status").innerHTML = "Game Is Loading";
+}
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
+	img = loadImage("mario05.png")
 }
 
 function setup() {
@@ -9,8 +23,30 @@ function setup() {
 	canvas.parent('canvas');
 	
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console')
 }
 
 function draw() {
 	game()
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+function game()
+{
+	console.log("noseX = " + noseX +" ,noseY = "+ noseY);
+	instializeInDraw();
+	moveEnvironment(mario);
+	drawSprites();
 }
